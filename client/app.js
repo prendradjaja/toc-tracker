2725623a969ca1fefffee8ae10e2b393e76b7f5c
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-07-27 21:27:16 -0700

    Home page!

--- client/app.js
+++ client/app.js
@@ -1,17 +1,49 @@
-const homePage = {template: `
-  <div>home</div>
-`};
+import { getBooks } from './api.js';
+
+const homePage = {
+  data: function () {
+    return {
+      books: undefined
+    };
+  },
+  template: `
+    <div>
+      <router-link to="/add">Add a book</router-link>
+      <ul v-if="books">
+        <li v-for="book in books" :key="book.id">
+          <router-link :to="'/book/' + book.id">
+            {{ book.title }}
+          </router-link>
+        </li>
+      </ul>
+      <ul v-else>
+        <li>...</li>
+      </ul>
+    </div>
+  `,
+  created: function () {
+    getBooks()
+      .then(books => {
+        this.books = books;
+      });
+  }
+};
 const bookPage = {template: `
   <div>book {{ $route.params.id }}</div>
 `};
+const addBookPage = {
+  template: `<div>add</div>`
+};
 
 
 const routes = [
   { path: '/', component: homePage },
-  { path: '/book/:id', component: bookPage }
+  { path: '/book/:id', component: bookPage },
+  { path: '/add', component: addBookPage },
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
