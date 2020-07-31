74dc748eaf36b0a11bfbcff9e31b98e3ba8e25f8
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-07-30 18:23:07 -0700

    Auth (#3)
    - Auth via passport-facebook
    - Add table `user_table` -- user is created upon auth
    - `book`s now have `owner_id REFERENCES user_table`
    - User can only access their own data

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import { Pool } from "pg";
+import * as passport from 'passport';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
 
 // Required variables
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -14,6 +16,84 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
 app.use(express.static('../client'));
+app.use(require('morgan')('combined'));
+
+// From Passport example -- not sure if all these dependencies are necessary
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({
+  secret: 'keyboard cat', // TODO ?
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.enable("trust proxy");
+
+
+passport.use(new FacebookStrategy(
+  {
+    clientID: process.env['FACEBOOK_CLIENT_ID'],
+    clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
+    callbackURL: '/return'
+  },
+  async function(accessToken, refreshToken, profile, done) {
+    const pgClient = await pgPool.connect();
+    try {
+      await pgClient.query('BEGIN');
+
+      let user = (await pgClient.query(`
+        SELECT *
+        FROM user_table
+        WHERE auth_provider = 'facebook'
+          AND id_from_provider = $1
+      `, [profile.id])).rows[0];
+
+      if (!user) {
+        user = (await pgClient.query(`
+          INSERT INTO
+            user_table(auth_provider, id_from_provider)
+          VALUES
+            ('facebook', $1)
+          RETURNING *
+        `, [profile.id])).rows[0];
+      }
+      await pgClient.query('COMMIT');
+      done(null, user);
+    } catch (err) {
+      await pgClient.query('ROLLBACK');
+      console.error(err);
+      done(true);
+    } finally {
+      pgClient.release();
+    }
+  }
+));
+
+
+passport.serializeUser(function(user, done) {
+  done(null, (user as any).id);
+});
+
+passport.deserializeUser(async function(id, done) {
+  try {
+    const user = (await pgPool.query(`
+      SELECT *
+      FROM user_table
+      WHERE id = $1
+    `, [id])).rows[0];
+    if (user) {
+      done(null, user);
+    } else {
+      console.error('passport.deserializeUser could not find user with id:', id)
+      done(true);
+    }
+  } catch (err) {
+    console.error(err);
+    done(true);
+  }
+});
 
 const pgPool = new Pool({
   connectionString: DATABASE_URL,
@@ -25,121 +105,182 @@ configureRoutes();
 app.listen(PORT, () => console.log('Example app listening at http://localhost:'+PORT))
 
 function configureRoutes() {
-  app.get('/api/books', async (req, res) => {
-    try {
-      await fakeNetworkDelay();
-      const { rows: books } = await pgPool.query(`
-        SELECT *
-        FROM book
-        ORDER BY id DESC
-      `);
-      res.send(books);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Error " + err);
-    }
+  app.get('/login/facebook',
+    passport.authenticate('facebook')
+  );
+
+  app.get('/return',
+    passport.authenticate('facebook', { failureRedirect: '/' }),
+    function(req, res) {
+      res.redirect('/');
   });
 
-  app.get('/api/books/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
+  app.get('/api/me',
+    async function(req, res, next) {
       await fakeNetworkDelay();
-      const title = (await pgPool.query(`
-        SELECT title
-        FROM book
-        WHERE id = $1
-      `, [id])).rows[0].title;
-      const { rows: chapters } = await pgPool.query(`
-        SELECT *
-        FROM chapter
-        WHERE book_id = $1
-        ORDER BY id ASC
-      `, [id]);
-      res.send({ title, chapters });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Error " + err);
+      next();
+    },
+    ensureLoggedIn,
+    function(req, res){
+      res.send({});
+      // Maybe actually send the user down -- not needed right now, since I'm just using this to
+      // check logged-in status.
     }
-  });
+  );
 
-  app.post('/api/books', async (req, res) => {
-    const { title, chapters } = req.body;
-    await fakeNetworkDelay();
-    const pgClient = await pgPool.connect();
-    try {
-      await pgClient.query('BEGIN');
-      const bookId = (await pgClient.query('INSERT INTO book(title) VALUES ($1) RETURNING id', [title])).rows[0].id;
-      // TODO Do this loop as one query?
-      for (let chapter of chapters) {
-        await pgClient.query('INSERT INTO chapter(book_id, title) VALUES ($1, $2)', [bookId, chapter]);
+  app.get('/api/books',
+    ensureLoggedIn,
+    async (req, res) => {
+      try {
+        await fakeNetworkDelay();
+        const { rows: books } = await pgPool.query(`
+          SELECT *
+          FROM book
+          WHERE owner_id = $1
+          ORDER BY id DESC
+        `, [getUserId(req)]);
+        res.send(books);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Error " + err);
       }
-      await pgClient.query('COMMIT')
-      res.send('{}')
-    } catch (err) {
-      await pgClient.query('ROLLBACK')
-      console.error(err);
-      res.status(500).send("Error " + err);
-    } finally {
-      pgClient.release();
     }
-  });
+  );
 
-  app.delete('/api/books/:id', async (req, res) => {
-    const { id } = req.params;
-    await fakeNetworkDelay();
-    const pgClient = await pgPool.connect();
-    try {
-      await pgClient.query('BEGIN');
-      await pgClient.query('DELETE FROM chapter WHERE book_id = $1', [id]);
-      await pgClient.query('DELETE FROM book WHERE id = $1', [id]);
-      await pgClient.query('COMMIT')
-      res.send('{}')
-    } catch (err) {
-      await pgClient.query('ROLLBACK')
-      console.error(err);
-      res.status(500).send("Error " + err);
-    } finally {
-      pgClient.release();
-    }
-  })
-
-  app.post('/api/chapters/:id/read', async (req, res) => {
-    try {
-      await fakeNetworkDelay();
+  app.get('/api/books/:id',
+    ensureLoggedIn,
+    async (req, res) => {
       const { id } = req.params;
-      const queryResult = await pgPool.query(`
-        UPDATE chapter
-        SET read_at = current_timestamp
-        WHERE id = $1
-      `, [id]);
-      if (!queryResult.rowCount) {
-        throw new Error("Chapter not found: ID " + id)
+      try {
+        await fakeNetworkDelay();
+        const { title, owner_id } = (await pgPool.query(`
+          SELECT title, owner_id
+          FROM book
+          WHERE id = $1
+        `, [id])).rows[0];
+        if (owner_id !== getUserId(req)) {
+          throw new Error(); // TODO In real life, should probably not even leak existence of others' data
+        }
+        const { rows: chapters } = await pgPool.query(`
+          SELECT *
+          FROM chapter
+          WHERE book_id = $1
+          ORDER BY id ASC
+        `, [id]);
+        res.send({ title, chapters });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Error " + err);
       }
-      res.send('{}')
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Error " + err);
     }
-  })
+  );
 
-  app.post('/api/chapters/:id/unread', async (req, res) => {
-    try {
+  app.post('/api/books',
+    ensureLoggedIn,
+    async (req, res) => {
+      const { title, chapters } = req.body;
       await fakeNetworkDelay();
-      const { id } = req.params;
-      const queryResult = await pgPool.query(`
-        UPDATE chapter
-        SET read_at = NULL
-        WHERE id = $1
-      `, [id]);
-      if (!queryResult.rowCount) {
-        throw new Error("Chapter not found: ID " + id)
+      const pgClient = await pgPool.connect();
+      try {
+        await pgClient.query('BEGIN');
+        const bookId = (await pgClient.query(`
+          INSERT INTO book(title, owner_id)
+          VALUES ($1, $2)
+          RETURNING id
+        `, [title, getUserId(req)])).rows[0].id;
+        // TODO Do this loop as one query?
+        for (let chapter of chapters) {
+          await pgClient.query('INSERT INTO chapter(book_id, title) VALUES ($1, $2)', [bookId, chapter]);
+        }
+        await pgClient.query('COMMIT')
+        res.send('{}')
+      } catch (err) {
+        await pgClient.query('ROLLBACK')
+        console.error(err);
+        res.status(500).send("Error " + err);
+      } finally {
+        pgClient.release();
       }
-      res.send('{}')
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Error " + err);
     }
-  })
+  );
+
+  app.delete('/api/books/:id',
+    ensureLoggedIn,
+    async (req, res) => {
+      const { id } = req.params;
+      await fakeNetworkDelay();
+      const pgClient = await pgPool.connect();
+      try {
+        await pgClient.query('BEGIN');
+        const { owner_id } = (await pgPool.query(`
+          SELECT owner_id
+          FROM book
+          WHERE id = $1
+        `, [id])).rows[0];
+        if (owner_id !== getUserId(req)) {
+          throw new Error(); // TODO In real life, should probably not even leak existence of others' data
+        }
+        await pgClient.query('DELETE FROM chapter WHERE book_id = $1', [id]);
+        await pgClient.query('DELETE FROM book WHERE id = $1', [id]);
+        await pgClient.query('COMMIT')
+        res.send('{}')
+      } catch (err) {
+        await pgClient.query('ROLLBACK')
+        console.error(err);
+        res.status(500).send("Error " + err);
+      } finally {
+        pgClient.release();
+      }
+    }
+  )
+
+  app.post('/api/chapters/:id/read',
+    ensureLoggedIn,
+    async (req, res) => {
+      try {
+        await fakeNetworkDelay();
+        const { id } = req.params;
+        const queryResult = await pgPool.query(`
+          UPDATE chapter as c
+          SET read_at = current_timestamp
+          FROM book as b
+          WHERE c.id = $1
+            AND b.owner_id = $2
+        `, [id, getUserId(req)]);
+        if (!queryResult.rowCount) {
+          throw new Error("Chapter not found: ID " + id)
+        }
+        res.send('{}')
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Error " + err);
+      }
+    }
+  )
+
+  app.post('/api/chapters/:id/unread',
+    ensureLoggedIn,
+    async (req, res) => {
+      try {
+        await fakeNetworkDelay();
+        const { id } = req.params;
+        const queryResult = await pgPool.query(`
+          UPDATE chapter as c
+          SET read_at = NULL
+          FROM book as b
+          WHERE c.id = $1
+            AND b.owner_id = $2
+        `, [id, getUserId(req)]);
+        if (!queryResult.rowCount) {
+          throw new Error("Chapter not found: ID " + id)
+        }
+        res.send('{}')
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Error " + err);
+      }
+    }
+  )
 }
 
 function fakeNetworkDelay() {
@@ -148,4 +289,19 @@ function fakeNetworkDelay() {
 
 function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Based on connect-ensure-login
+ */
+function ensureLoggedIn(req, res, next) {
+  if (!req.isAuthenticated || !req.isAuthenticated()) {
+    res.status(403).send("{}");
+    return;
+  }
+  next();
+}
+
+function getUserId(req: express.Request): number {
+  return (req.user as any).id;
 }
